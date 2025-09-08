e8ff4aaf0dac4b9a3c168f195f10055fe6a65dc1
Frederik St-Onge <frederik.stonge@gmail.com>
2025-09-08 10:09:14 -0400

    Remove secure storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
-import 'package:crypto/crypto.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_secure_storage_x/flutter_secure_storage_x.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:path_provider/path_provider.dart';
@@ -12,26 +8,9 @@ import 'bootstrapper.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
-
-  final storage = FlutterSecureStorage(aOptions: const AndroidOptions(dataStore: true));
-
-  const passwordKey = 'HydratedStoragePassword';
-  final hasPassword = await storage.containsKey(key: passwordKey);
-
-  final password = hasPassword
-      ? await storage.read(key: passwordKey)
-      : base64Url.encode(List<int>.generate(32, (i) => i + DateTime.now().millisecond));
-
-  if (!hasPassword && password != null) {
-    await storage.write(key: passwordKey, value: password);
-  }
-
   final storagePath = await getApplicationDocumentsDirectory();
 
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: HydratedStorageDirectory(storagePath.path),
-    encryptionCipher: password != null ? HydratedAesCipher(sha256.convert(utf8.encode(password)).bytes) : null,
-  );
+  HydratedBloc.storage = await HydratedStorage.build(storageDirectory: HydratedStorageDirectory(storagePath.path));
 
   runApp(const Bootstrapper());
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_secure_storage_x_macos
 import media_kit_libs_macos_video
 import media_kit_video
 import package_info_plus
@@ -15,7 +14,6 @@ import volume_controller
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
