44af9bbe0a8217f0bbbe9a8b7492ffc9aaddfc92
Frederik St-Onge <frederik.stonge@gmail.com>
2025-09-08 09:35:58 -0400

    Add secure storage for hydrated cubits

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_secure_storage_x/flutter_secure_storage_x.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:path_provider/path_provider.dart';
@@ -9,8 +13,24 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
 
+  final storage = FlutterSecureStorage(aOptions: const AndroidOptions(dataStore: true));
+
+  const passwordKey = 'HydratedStoragePassword';
+  final hasPassword = await storage.containsKey(key: passwordKey);
+
+  final password = hasPassword
+      ? await storage.read(key: passwordKey)
+      : base64Url.encode(List<int>.generate(32, (i) => i + DateTime.now().millisecond));
+
+  if (!hasPassword && password != null) {
+    await storage.write(key: passwordKey, value: password);
+  }
+
+  final storagePath = await getApplicationDocumentsDirectory();
+
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: HydratedStorageDirectory((await getApplicationDocumentsDirectory()).path),
+    storageDirectory: HydratedStorageDirectory(storagePath.path),
+    encryptionCipher: password != null ? HydratedAesCipher(sha256.convert(utf8.encode(password)).bytes) : null,
   );
 
   runApp(const Bootstrapper());
