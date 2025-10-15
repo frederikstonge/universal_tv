6b961823eee1329c4aeb93e9f0117407b5c82bde
Frederik St-Onge <frederik.stonge@gmail.com>
2025-10-15 15:25:54 -0400

    Add web

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:media_kit/media_kit.dart';
@@ -9,8 +10,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
 
-  final storagePath = await getApplicationDocumentsDirectory();
-  HydratedBloc.storage = await HydratedStorage.build(storageDirectory: HydratedStorageDirectory(storagePath.path));
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getApplicationDocumentsDirectory()).path),
+  );
 
   runApp(const Bootstrapper());
 }
